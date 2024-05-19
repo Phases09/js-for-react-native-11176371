c40abfdd6294147e55abcf6d02a91278712ca46d
Phases09 <pkanaman001@st.ug.edu.gh>
2024-05-19 15:17:56 +0100

    Update arrayManipulation.js

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -9,17 +9,6 @@ function processArray(arr) {
   }
   return result;
 }
-function processArray(arr) {
-  let result = [];
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] % 2 === 0) {
-      result.push(arr[i] * arr[i]);
-    } else {
-      result.push(arr[i] * 3);
-    }
-  }
-  return result;
-}
 
 function formatArrayStrings(strArr, numArr) {
   let result = [];
