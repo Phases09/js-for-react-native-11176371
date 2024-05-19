563a48e8adf74d6425fad93f673e3fd7c616ec1d
Phases09 <pkanaman001@st.ug.edu.gh>
2024-05-19 13:39:54 +0100

    Update arrayManipulation.js

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -9,3 +9,26 @@ function processArray(arr) {
   }
   return result;
 }
+function processArray(arr) {
+  let result = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0) {
+      result.push(arr[i] * arr[i]);
+    } else {
+      result.push(arr[i] * 3);
+    }
+  }
+  return result;
+}
+
+function formatArrayStrings(strArr, numArr) {
+  let result = [];
+  for (let i = 0; i < strArr.length; i++) {
+    if (numArr[i] % 2 === 0) {
+      result.push(strArr[i].toUpperCase());
+    } else {
+      result.push(strArr[i].toLowerCase());
+    }
+  }
+  return result;
+}
